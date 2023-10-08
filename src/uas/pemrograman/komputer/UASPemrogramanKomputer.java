7d368a24d34ef627c4841f7deba2966927daf785
@IlhamRichie <LENOVO@LEPNOPOKU>
2023-10-08 20:19:35 +0700

    UAS 22090098

--- src/uas/pemrograman/komputer/UASPemrogramanKomputer.java
+++ src/uas/pemrograman/komputer/UASPemrogramanKomputer.java
@@ -14,6 +14,7 @@ public class UASPemrogramanKomputer {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        System.out.println("Tes Repo");
         // TODO code application logic here
     }
     
