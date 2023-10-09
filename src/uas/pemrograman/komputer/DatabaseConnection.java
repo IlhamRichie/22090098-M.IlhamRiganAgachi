21f22fa79cc6bc81d3f752e30d9cc5bc60d4166b
@IlhamRichie <LENOVO@LEPNOPOKU>
2023-10-09 13:24:05 +0700

    UAS 22090098

--- src/uas/pemrograman/komputer/DatabaseConnection.java
+++ src/uas/pemrograman/komputer/DatabaseConnection.java
@@ -6,13 +6,25 @@ package uas.pemrograman.komputer;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import javax.swing.JOptionPane;
 
 /**
  *
  * @author LENOVO
  */
 public class DatabaseConnection {
+    Connection cn;
+   
     public static Connection connect() {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection cn = DriverManager.getConnection("jdbc:mysql://localhost:3306/data_umkm","root","");
+            return cn;
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(Null, e);
+        }
+                
+      
         Connection connection = null;
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
