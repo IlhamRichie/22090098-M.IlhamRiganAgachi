30e13f1fbb08ce064b6bfb029cf881d2e4441045
@IlhamRichie <LENOVO@LEPNOPOKU>
2023-10-27 20:41:35 +0700

    UAS 22090098

--- src/uas/pemrograman/komputer/DatabaseConnection.java
+++ src/uas/pemrograman/komputer/DatabaseConnection.java
@@ -1,42 +1,34 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package uas.pemrograman.komputer;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import javax.swing.JOptionPane;
 
-/**
- *
- * @author LENOVO
- */
+import com.mysql.cj.jdbc.MysqlDataSource; // Tambahkan import ini
+import java.sql.Connection;
+import java.sql.SQLException;
+
 public class DatabaseConnection {
-    Connection cn;
-   
-    public static Connection connect() {
+    public static Connection sambungkeDB() {
         try {
-            Class.forName("com.mysql.jdbc.Driver");
-            Connection cn = DriverManager.getConnection("jdbc:mysql://localhost:3306/data_umkm","root","");
-            return cn;
-        } catch (Exception e) {
-            JOptionPane.showMessageDialog(Null, e);
+            MysqlDataSource mds = new MysqlDataSource();
+            mds.setUser("root");
+            mds.setPassword("");
+            mds.setDatabaseName("data_umkm");
+            mds.setPortNumber(3306);
+            mds.setServerName("localhost");
+            mds.setServerTimezone("Asia/Jakarta");
+            Connection con = mds.getConnection();
+            return con;
+        } catch (SQLException e) {
+            System.err.println(e.getMessage());
+            e.printStackTrace(); // Tambahkan ini untuk melihat trace error secara lengkap
         }
-                
-      
-        Connection connection = null;
-        try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            String url = "jdbc:mysql://localhost:3306/data_umkm";
-            String username = "root@localhost";
-            String password = "kata_sandi";
-            connection = DriverManager.getConnection(url, username, password);
-            System.out.println("Database connected!");
-        } catch (ClassNotFoundException | SQLException e) {
-            e.printStackTrace();
+        return null;
+    }
+
+    public static void main(String[] args) {
+        Connection c = sambungkeDB();
+        if (c != null) {
+            System.out.println("Terhubung");
+        } else {
+            System.out.println("Gagal terhubung");
         }
-        return connection;
     }
 }
-
